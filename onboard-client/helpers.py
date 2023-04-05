37f5dd086c4fb5d66de52a4aa28bf195d6f01eda
240062247 <ryan.bell@geappliances.com>
2023-04-05 14:55:32 -0400

    tweaks and fixes

--- onboard-client/helpers.py
+++ onboard-client/helpers.py
@@ -4,29 +4,31 @@ import subprocess
 import re
 import zipfile
 
+
 def url_response(url):
     username = os.getlogin()
-    parentPath = "C:/Users/"+username+"/Downloads/"
+    parentPath = "C:/Users/" + username + "/Downloads/"
     newDir = "Onboarding"
-    dirPath = os.path.join(parentPath,newDir)
-    if os.path.exists(dirPath) == False:
+    dirPath = os.path.join(parentPath, newDir)
+    if not os.path.exists(dirPath):
         os.mkdir(dirPath)
-    
+
     name, url = url
-    downPath = os.path.join(parentPath,newDir,name)
+    downPath = os.path.join(parentPath, newDir, name)
     try:
-        r = requests.get(url, stream = True)
+        r = requests.get(url, stream=True)
         with open(downPath, 'wb') as f:
             for ch in r:
                 f.write(ch)
     except Exception as e:
         print(e)
 
+
 def java_install():
     username = os.getlogin()
-    parentPath = "C:/Users/"+username+"/Downloads/"
+    parentPath = "C:/Users/" + username + "/Downloads/"
     newDir = "Onboarding"
-    dirPath = os.path.join(parentPath,newDir)
+    dirPath = os.path.join(parentPath, newDir)
     os.chdir(dirPath)
     pattern = '\"(\d+\.\d+\.\d+).*\"'
 
@@ -37,102 +39,158 @@ def java_install():
     if "11.0" not in version:
         # do not uncomment until ready to install
         # subprocess.run("jdk-11_windows-x64_bin.exe /s")
-        set_env("JAVA_HOME")
+        set_java_env()
         if 'java version "' in subprocess.run("java -version", capture_output=True, text=True).stderr:
             print("Java has been installed successfully")
     if "11.0" in version:
         print("Java is already installed")
 
+
 def maven_install():
     username = os.getlogin()
-    parentPath = "C:/Users/"+username+"/Downloads/Onboarding/"
+    parentPath = "C:/Users/" + username + "/Downloads/Onboarding/"
     newDir = "C:/Program Files/Maven/"
-    if os.path.exists(newDir) == False:
+    if not os.path.exists(newDir):
         os.mkdir(newDir)
     for x in os.listdir(parentPath):
         if "apache-maven" in x:
             presentDir = os.path.join(parentPath, x)
     with zipfile.ZipFile(presentDir, 'r') as zip_ref:
         zip_ref.extractall(newDir)
-    
+
     check = subprocess.run("mvn -version", capture_output=True, text=True, shell=True).stdout.splitlines()[0]
     if "Apache Maven 3" not in check:
         print("Maven is not present")
-        set_env("MAVEN_HOME")
+        set_maven_env()
     if "Apache Maven 3" in check:
         print("Maven is present")
-        
+
+
 def git_install():
     username = os.getlogin()
-    parentPath = "C:/Users/"+username+"/Downloads/Onboarding/"
+    parentPath = "C:/Users/" + username + "/Downloads/Onboarding/"
     for x in os.listdir(parentPath):
         if "Git-" in x:
             command = x + " /VERYSILENT /NORESTART"
-    # subprocess.run(command) ------- uncomment when ready to install
-    if "git version 2" in subprocess.run("git version", capture_output=True, text=True).stdout:
-        print("Git is installed")
-        
+    if "git version 2" not in subprocess.run("git version", capture_output=True, text=True).stdout:
+        subprocess.run(command)
+        print("Git has been installed")
+    elif "git version 2" in subprocess.run("git version", capture_output=True, text=True).stdout:
+        print("Git is already installed")
+
+
 def slack_install():
     username = os.getlogin()
-    parentPath = "C:/Users/"+username+"/Downloads/Onboarding/"
-    checkPath = "C:/Users/"+username+"/AppData/Local/slack/slack.exe"
+    parentPath = "C:/Users/" + username + "/Downloads/Onboarding/"
+    checkPath = "C:/Users/" + username + "/AppData/Local/slack/slack.exe"
     command = os.path.join(parentPath, "SlackSetup.exe") + " -s"
-    if os.path.exists(checkPath) == False:
+    if not os.path.exists(checkPath):
         subprocess.run(command)
         print("Slack has been installed")
-    elif os.path.exists(checkPath) == True:
+    elif os.path.exists(checkPath):
         print("Slack is already installed")
-        
+
+
 def intellij_install():
     username = os.getlogin()
-    parentPath = "C:/Users/"+username+"/Downloads/Onboarding/"
-    checkPath = "C:/Program Files/JetBrains/"
+    parentPath = "C:/Users/" + username + "/Downloads/Onboarding/"
+    checkPath = "C:/Program Files/JetBrains/IntelliJ"
     for x in os.listdir(parentPath):
         if "ideaIC-" in x:
             command = x + " /S"
-    if os.path.exists(checkPath) == False:
+    if not os.path.exists(checkPath):
         subprocess.run(command)
         print("IntelliJ has been installed")
-    elif os.path.exists(checkPath) == True:
+    elif os.path.exists(checkPath):
         print("IntelliJ is already installed")
-        
+
+
 def python_install():
     username = os.getlogin()
-    parentPath = "C:/Users/"+username+"/Downloads/Onboarding/"
-    checkPath = "C:/Users/"+username+"/AppData/Local/Programs/Python/"
+    parentPath = "C:/Users/" + username + "/Downloads/Onboarding/"
+    checkPath = "C:/Users/" + username + "/AppData/Local/Programs/Python/"
     for x in os.listdir(parentPath):
         if "python-3" in x:
-            command = x + " /quiet" # todo: figure out how to add this to PATH - cannot be done via installer switches
-    if os.path.exists(checkPath) == False:
+            command = x + " /quiet"  # todo: figure out how to add this to PATH - cannot be done via installer switches
+    if not os.path.exists(checkPath):
         subprocess.run(command)
         print("Python has been installed")
-    elif os.path.exists(checkPath) == True:
+    elif os.path.exists(checkPath):
         print("Python is already installed")
-        
+
+
 def set_java_env():
-    var_name = "JAVA_HOME"
     java_dir = "C:/Program Files/Java/"
-    if os.environ.get(var_name) == None: # set JAVA_HOME if it doesn't already exist
-        for x in os.listdir(java_dir): # loop through and grab the full path name of jdk directory
+    var_name = "JAVA_HOME "
+    command_prefix = 'setx '
+    command_suffix = '" /m'
+    java_check = "echo %JAVA_HOME%"
+    
+    java_res = subprocess.run(java_check, capture_output=True, shell=True, text=True).stdout
+
+    if java_res == "%JAVA_HOME%":  # set JAVA_HOME if it doesn't already exist
+        for x in os.listdir(java_dir):  # grab the full path name of jdk directory
             if "jdk-11" in x:
                 target = os.path.join(java_dir, x)
-        os.environ[var_name] = target
+        
+        command = command_prefix + var_name + ' "' + target + command_suffix
+        subprocess.run(command)
         print("JAVA_HOME has been created")
-    elif os.environ.get(var_name) != None:
+    
+    elif "jdk-11" in java_res:
         print("JAVA_HOME already exists")
-      
+
+
 def set_maven_env():
-    var_name = "MAVEN_HOME"      
-    if os.environ.get("JAVA_HOME") == None: # if JAVA_HOME does not exist, create it
+    mvn_dir = "C:/Program Files/Maven/"
+    var_name = "MAVEN_HOME "
+    command_prefix = 'setx '
+    command_suffix = '" /m'
+    java_check = "echo %JAVA_HOME%"
+    maven_check = "echo %MAVEN_HOME%"
+    
+    java_res = subprocess.run(java_check, capture_output=True, shell=True, text=True).stdout
+    maven_res = subprocess.run(maven_check, capture_output=True, shell=True, text=True).stdout
+    
+    if java_res == "%JAVA_HOME%":
         print("JAVA_HOME is not set")
-        print("MAVEN_HOME is not set")
-    elif os.environ.get("JAVA_HOME") != None: # if JAVA_HOME does exist
-        mvn_dir = "C:/Program Files/Maven/"
-        if os.environ.get(var_name) == None: # set MAVEN_HOME if it doesn't already exist
-            for x in os.listdir(mvn_dir): # loop through and grab the full path name of apache-maven directory
+        print("MAVEN_HOME could not be set")
+    
+    elif "jdk" in java_res:
+        if maven_res == "%MAVEN_HOME%":  # set MAVEN_HOME if it doesn't already exist
+            for x in os.listdir(mvn_dir):  # grab the full path name of apache-maven directory
                 if "apache-maven" in x:
                     target = os.path.join(mvn_dir, x)
-            os.environ[var_name] = target
+            
+            command = command_prefix + var_name + ' "' + target + command_suffix
+            subprocess.run(command)
             print("MAVEN_HOME has been created")
-        elif os.environ.get(var_name) != None:
+        
+        elif "apache-maven" in maven_res:
             print("MAVEN_HOME already exists")
+   
+            
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
+def set_test_env():
+    var_name = "TEST_HOME "
+    test_dir = "C:/Program Files/Test/"
+    command_prefix = 'setx '
+    command_suffix = '" /m'
+    if os.environ.get(var_name) is None:  # set TEST_HOME if it doesn't already exist
+        for x in os.listdir(test_dir):  # loop through and grab the full path name of test-directory
+            if "test-directory" in x:
+                target = os.path.join(test_dir, x)
+        command = command_prefix + var_name + ' "' + target + command_suffix
+        subprocess.run(command)
+        if os.environ.get(var_name) is None:
+            print("TEST_HOME could not be created")
+        elif os.environ.get(var_name) is not None:
+            print("TEST_HOME has been created")
+    elif os.environ.get(var_name) is not None:
+        print("TEST_HOME already exists")
--- onboard-client/main.py
+++ onboard-client/main.py
@@ -1,14 +1,16 @@
 import helpers
 import requests
+import ctypes
+import sys
 
 baseUrl = "https://raw.githubusercontent.com/"
 grabPath = "ryan-bell-gea/onboarding.v1/main/README.md"
 urls = []
 
-#hit the GH repo and download the contents as a list of strings, each string being one line of the README
+# hit the GH repo and download the contents as a list of strings, each string being one line of the README
 contentsList = requests.get(baseUrl+grabPath).text.splitlines()
 
-#pull urls from each line and split filename from url, download each and place in User's Downloads/Onboarding/
+# pull urls from each line and split filename from url, download each and place in User's Downloads/Onboarding/
 for url in contentsList:
     if url.startswith("source"):
         url = url.split("=")[1]
@@ -16,15 +18,25 @@ for url in contentsList:
         pack = (name, url)
         urls.append(pack)
 
-#hit each link from the GH README and download the file
+# hit each link from the GH README and download the file
 for x in urls:
     helpers.url_response(x)
 
-# helpers.git_install()
-# helpers.slack_install()
-# helpers.python_install()
-# helpers.java_install()
-# helpers.set_java_env("JAVA_HOME")
-# helpers.maven_install()
-# helpers.set_maven_env("MAVEN_HOME")
-# helpers.intellij_install()
+helpers.git_install()
+helpers.slack_install()
+# helpers.python_install() -- this is not ready
+# helpers.java_install() -- need to update for msi install
+helpers.maven_install()
+
+# need elevated permissions for setting env vars
+if helpers.is_admin():
+    helpers.set_java_env()
+    helpers.set_maven_env()
+else:
+    # prompt user to elevate
+    ctypes.windll.shell32.ShellExecuteW(None, u"runas", sys.executable, " ".join(sys.argv), None, 1)
+    helpers.set_java_env()
+    helpers.set_maven_env()
+
+# install after java and maven env vars are set - do not need elevation
+helpers.intellij_install()
