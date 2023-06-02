29c3d8f1c1960664a2c4ad6b1a29b9c546278564
Ryan Bell <ryan.bell@geappliances.com>
2023-06-02 09:05:42 -0400

    update

--- config.ps1
+++ config.ps1
@@ -1,8 +1,2 @@
-$batfile = choco.bat
-Write-Host "Beginning the onboarding process. Please do not close this terminal until it is complete."
-Write-Host "Installing Chocolatey, a universal package manager for Windows."
-
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-Write-Host "Chocolatey installation complete!"
-Start-Process $batfile -Verb RunAs
+Start-Process choco.bat -Verb RunAs
